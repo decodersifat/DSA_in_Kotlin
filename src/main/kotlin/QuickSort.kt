b6292686e800d0a606abd9cf227054fc70cfd1c7
decodersifat <user.sifathossain@gmail.com>
2023-11-03 22:06:45 +0600

    Initial commit

--- src/main/kotlin/QuickSort.kt
+++ src/main/kotlin/QuickSort.kt
@@ -1,4 +1,44 @@
-fun main (){
 
-    println("This is QuickSort")
+//Creating a function for partition, and it will return the index of exact location of pivot which will be j
+//partition function wil take 3 parameter: high, low and the array we have to sort.
+fun partition(l:Int,h:Int,arr:IntArray):Int{
+    var i = l
+    var j = h
+    var pivot = arr[l]
+
+    while (i<j){
+        while (arr[i]<pivot){
+            i++
+        }
+        while (arr[j]>pivot){
+
+            j--
+        }
+        if (i>j){
+            return j
+        }
+        // Swap
+        var temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+
+        //Increasing low element index
+        i++
+        //Decreasing upper element index
+        j--
+
+
+    }
+
+    return j
+
+}
+
+
+fun main (){
+    //Creating an array
+    var arr = intArrayOf(3,4,1,5,6,7,9,8,2)
+    var low = 0
+    var high = arr.size -1
+
 }
